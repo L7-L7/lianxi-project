acf755f61e08dcaba16b9a7a41018d48428691c6
L7-L7 <1837564777@qq.com>
2023-03-18 23:02:07 +0800

    函数的递归

---
+++ test-3-18/test-3-18/text.c
@@ -0,0 +1,181 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int Fib(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+
+	while (n > 2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+	return c;
+}
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = Fib(n);
+	printf("ret=%d\n", ret);
+	return 0;
+}
+
+
+
+
+
+
+////描述第n个斐波那契数的时候
+//int Fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return Fib(n - 1) + Fib(n - 2);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = Fib(n);
+//	printf("ret=%d\n", ret);
+//	return 0;
+//}
+
+
+
+
+
+
+//int Fac2(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * Fac2(n - 1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = Fac2(n);
+//	printf("%d", ret);
+//	return 0;
+//}
+
+
+
+
+
+
+
+//int Fac1(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = Fac1(n);
+//	printf("%d", ret);
+//	return 0;
+//}
+
+
+
+
+
+
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//	{
+//		return 1 + my_strlen(str + 1);
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//	int len = my_strlen(arr);
+//	printf("len=%d\n", len);
+//	return 0;
+//}
+
+
+
+
+
+
+//#include<string.h>
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str  != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//	//int len=strlen(arr);//求字符串长度
+//	//printf("%d\n", len);
+//	int len = my_strlen(arr);// arr是数组，数组传参，传过去的是第一个元素的地址
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+
+
+
+//void print(int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);//1234
+//	//递归
+//	print(num);
+//	return 0;
+//}
+
+
+
+
+//#include"add.h"
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
